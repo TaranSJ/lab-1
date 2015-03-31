548cb26bcc7590e8d7dcacc571c0f3b3658047b3
TaranSJ <taran-1995@bk.ru>
2015-03-31 18:29:27 +0200

    lab-1

---
+++ OSiSP/ClassLibrary1/Generic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    //new generic test class for our lab
+    public class Generic<T>
+    {
+        private T item;
+        //add item of some type 
+        public void Add(T item)
+        {
+            this.item = item;
+        }
+        //show item of some type
+        public T GetItem()
+        {
+            return this.item;
+        }
+    }
+}
---
+++ OSiSP/ClassLibrary1/tv.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    // extended generic class for test contravariation
+    public class tv : room
+    {
+        public string Name;
+        // method which never used but needed for show difference between classes
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
